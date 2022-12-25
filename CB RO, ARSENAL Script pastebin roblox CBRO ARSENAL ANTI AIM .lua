919aacef6d50f4db7cbb9d40d2e2a734bc3d221f
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:44 +0100

    commit zero

---
+++ CB RO, ARSENAL Script pastebin roblox CBRO ARSENAL ANTI AIM .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+game.RunService.Stepped:Connect(function() Game:GetService("ReplicatedStorage").Events.ControlTurn:FireServer(math.random(-1000,1000)) end)
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							This script gives you the resistance to play in aim in CB RO, Arsenal modes
+						
+]]
